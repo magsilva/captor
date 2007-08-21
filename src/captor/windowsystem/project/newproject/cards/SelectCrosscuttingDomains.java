f8e2b4b50deb5ba4aa4177ea65c02634e92d784a
cafpj <cafpj@af7bf151-d49d-4a85-9d03-aee0806bfb2f>
2007-08-21 18:14:38 +0000

    *** empty log message ***

---
+++ src/captor/windowsystem/project/newproject/cards/SelectCrosscuttingDomains.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package captor.windowsystem.project.newproject.cards;
+
+import captor.modelsystem.Model;
+import captor.modelsystem.Project;
+
+/**
+ * @author User
+ *
+ */
+public class SelectCrosscuttingDomains extends FITCard {
+
+	
+	public SelectCrosscuttingDomains(Model model, Project project){
+		super(model,project);
+	}
+	
+	/* (non-Javadoc)
+	 * @see captor.windowsystem.project.newproject.cards.FITCard#create()
+	 */
+	@Override
+	public void create() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see captor.windowsystem.project.newproject.cards.FITCard#validateFields()
+	 */
+	@Override
+	public boolean validateFields() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
