86d7bf497784636ad6de8defcbc9bd49436d45a6
magsilva <magsilva@af7bf151-d49d-4a85-9d03-aee0806bfb2f>
2007-09-30 15:34:07 +0000

    - Atualizado para o Jaxb 2.1. - A configuração do pacote destino de cada mapeamento agora está definida no próprio mapeamento.

---
+++ doc/scripts/jaxb/xmlxb.sh
@@ -0,0 +1,7 @@
+# export CLASSPATH=$CLASSPATH:/opt/jaxb-ri-20070917/lib/jaxb-api.jar:/opt/jaxb-ri-20070917/lib/jaxb-impl.jar:/opt/jaxb-ri-20070917/lib/jaxb-xjc.jar
+# export PATH=$PATH:/opt/jaxb-ri-20070917/bin
+# xjc.sh -p captor.domainsystem domain.xsd -d tmp -classpath /home/magsilva/Projects-ICMC/Captor/bin
+# xjc.sh -p captor.projectsystem.build.mapper rules.xsd -d tmp -classpath /home/magsilva/Projects-ICMC/Captor/bin
+
+xjc.sh domain.xsd -d tmp -classpath /home/magsilva/Projects-ICMC/Captor/bin
+xjc.sh rules.xsd -d tmp -classpath /home/magsilva/Projects-ICMC/Captor/bin
